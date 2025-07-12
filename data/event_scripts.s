72ebc6ca12dd193677b4e9b53da67065ee2596c9
Monad <70017393+Maqu12345@users.noreply.github.com>
2025-07-12 17:02:43 +0200

    Expanded Scorched Slab

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/ScorchedSlab_B1F/scripts.inc"
+
+	.include "data/maps/ScorchedSlab_B2F/scripts.inc"
