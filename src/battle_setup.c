fffb1567f05a6d6d5d582fc5d80817b9a0faf60c
Monad <70017393+Maqu12345@users.noreply.github.com>
2025-07-19 16:14:02 +0200

    Added items and Heatran to Scorched Slab

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -506,6 +506,9 @@ void BattleSetup_StartLegendaryBattle(void)
     case SPECIES_MEW:
         CreateBattleStartTask(B_TRANSITION_GRID_SQUARES, MUS_VS_MEW);
         break;
+    case SPECIES_HEATRAN:
+        CreateBattleStartTask(B_TRANSITION_BLACKHOLE, MUS_VS_KYOGRE_GROUDON);
+        break;
     }
 
     IncrementGameStat(GAME_STAT_TOTAL_BATTLES);
